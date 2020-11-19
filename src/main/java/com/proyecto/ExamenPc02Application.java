11151023cc657ad49fedee27a80ed1d4d78b0f5c
Crhistina <crhistina.javier.15@gmail.com>
2020-11-19 16:49:52 -0500

    Libro movil

---
+++ src/main/java/com/proyecto/ExamenPc02Application.java
@@ -0,0 +1,13 @@
+package com.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenPc02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenPc02Application.class, args);
+	}
+
+}
